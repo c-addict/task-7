67704595e0a46ac6453b40f0f686a83b8f7e6467
Nikolay Zelenev <zelenev.kolya.177@gmail.com>
2020-05-12 22:07:24 +0300

    added route methods

--- app.py
+++ app.py
@@ -1,11 +1,25 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
+def main():
     return render_template('index.html')
+
+
+@app.route('/search/', methods=['GET', 'POST'])
+def search():
+    if request.method == 'GET':
+        return render_template('search.html')
+    else:
+        return render_template('search.html', files=[])
+
+
+@app.route('/help/')
+def help_():
+    return render_template('help.html')
 
 
 if __name__ == '__main__':
