65fcaef7807d7b5c30b4685108790838e031977b
Nikolay Zelenev <zelenev.kolya.177@gmail.com>
2020-05-12 22:18:40 +0300

    added declaration of method that solves the problem of finding files

---
+++ utils/fileutils.py
@@ -0,0 +1,10 @@
+
+
+def search_files(pattern: str, dirs=None) -> list:
+    """
+    Returns a list of files whose names match the pattern parameter.
+    :param pattern:
+    :param dirs:
+    :return:
+    """
+    pass
